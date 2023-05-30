4c9f9eea497e7ecdc2071cd7dc753cfac2bbb5d8
Ivy-aminga <amingaivy@gmail.com>
2023-05-30 19:14:22 +0300

    gadgets API

---
+++ gadgets.js
@@ -0,0 +1,28 @@
+function generateProductCards(data) {
+  const productContainer = document.getElementById("products");
+  productContainer.classList.add("product-container"); // Add CSS class for flex display
+  data.forEach((product) => {
+    const card = document.createElement("div");
+    card.classList.add("product-card");
+    card.innerHTML = `
+      <img src="${product.thumbnail}" alt="${product.name}" class="product-image" />
+      <h2>${product.title}</h2>
+      <div class='productPrice'>
+        <p>${product.price}</p>
+        <button>ADD TO CART</button>
+      </div>
+    `;
+    productContainer.appendChild(card);
+  });
+}
+
+function fetchProducts() {
+  fetch("https://dummyjson.com/products")
+    .then((response) => response.json())
+    .then((data) => {
+      const returnedData = data.products.slice(0, 10);
+      generateProductCards(returnedData);
+    });
+}
+
+fetchProducts();
